2ee5053c97bc911fc3e10ceb31a7111474fcb24a
TarunISCO <201552063@iiitvadodara.ac.in>
2017-11-14 00:52:09 +0530

    Added News Use Case

--- Crop Management.py
+++ Crop Management.py
@@ -1,10 +1,13 @@
+import urllib
+import requests
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from flask import json
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 from flask.ext.googlemaps import GoogleMaps
-
+from app_data import states
 
 app = Flask(__name__)
 
@@ -37,6 +40,7 @@ class RegisterForm(Form):
     confirm = PasswordField('Confirm Password')
 
 
+#Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
@@ -64,6 +68,8 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
+
+# Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -91,7 +97,7 @@ def login():
                 session['username'] = name
 
                 flash('You are now logged in', 'success')
-                return redirect(url_for('dashboard'))
+                return redirect(url_for('index'))
             else:
                 error = 'Invalid login'
                 return render_template('login.html', error=error)
@@ -102,6 +108,26 @@ def login():
             return render_template('login.html', error=error)
 
     return render_template('login.html')
+
+# Mazor Crops
+@app.route('/mazor_crops')
+def mazor_crops():
+    # Create Cursor
+    cur = mysql.connection.cursor()
+
+    # Get mazor crops
+    result = cur.execute("SELECT * FROM crops")
+
+    crops = cur.fetchall()
+
+    if result > 0:
+        return render_template('mazor_crops.html', mazor_crops=crops)
+    else:
+        msg = 'No Articles Found'
+        return render_template('mazor_crops.html', msg=msg)
+    # Close connection
+    cur.close()
+
 
 # Check if user logged in
 def is_logged_in(f):
@@ -130,16 +156,57 @@ def warehouse():
 
     return render_template('warehouse.html')
 
-# Dashboard
-@app.route('/dashboard')
-@is_logged_in
-def dashboard():
-
-    return render_template('dashboard.html')
 
 @app.route('/policies')
 def policies():
     return render_template('policies.html')
+
+@app.route('/laboratories', methods=['GET', 'POST'])
+def laboratories():
+    if request.method == 'POST':
+        state = request.form['state']
+        # Create Cursor
+        cur = mysql.connection.cursor()
+
+        # Get mazor crops
+        result = cur.execute("SELECT * FROM diagnosticlabs WHERE State_Name = %s", [state])
+
+        labs = cur.fetchall()
+
+        if result > 0:
+            return render_template('state_labs.html', state=state, labs=labs)
+        else:
+            msg = 'No Labs found'
+            return render_template('state_labs.html', state=state, msg=msg)
+        # Close connection
+        cur.close()
+
+        return render_template('state_labs.html',state=state)
+    return render_template('laboratories.html', states=states)
+
+@app.route('/testinglabs', methods=['GET', 'POST'])
+def testinglabs():
+    if request.method == 'POST':
+        state = request.form['state']
+        url_tag = state.replace(" ", "-")
+        return render_template('test_labs.html',state=state)
+    return render_template('testinglabs.html', states=states)
+
+@app.route('/market', methods=['POST','GET'])
+def trend():
+    r = requests.get('https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=latest&apiKey=e77d6e63c62547d5a6d0adc4d5cad012')
+    response = r.text
+    data = json.loads(response)
+    news = data['articles']
+    if 'ok' in data['status']:
+        return render_template('news.html', news=news)
+    else:
+        msg = 'Sorry!! Try Later'
+        return render_template('news.html', msg=msg)
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
 
 if __name__ == '__main__':
     app.secret_key = 'secret123'
